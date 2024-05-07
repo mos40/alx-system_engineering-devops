f78eacde4c16b4b0a827ad93f9c2c2162967d44b
Moses <mosesmkhonza@outlook.com>
2024-05-07 10:21:35 -0700

    changes

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,31 +1,24 @@
 #!/usr/bin/python3
-"""
-1-top_ten
-"""
+'''
+    this module contains the function top_ten
+'''
 import requests
+from sys import argv
+
 
 def top_ten(subreddit):
-    """Prints the titles of the first 10 hot posts listed for a given subreddit"""
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
+    '''
+        returns the top ten posts for a given subreddit
+    '''
+    user = {'User-Agent': 'Lizzie'}
+    url = requests.get('https://www.reddit.com/r/{}/hot/.json?limit=10'
+                       .format(subreddit), headers=user).json()
+    try:
+        for post in url.get('data').get('children'):
+            print(post.get('data').get('title'))
+    except Exception:
+        print(None)
 
-    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"}
 
-    response = requests.get(url, headers=headers)
-
-    # Check if the response is successful (status code 200)
-    if response.status_code == 200:
-        data = response.json()  # Parse the JSON response
-        posts = data.get('data', {}).get('children', [])  # Extract the list of posts
-
-        # Iterate over the posts and print their titles
-        for post in posts:
-            print(post['data']['title'])
-    else:
-        print(None)  # Print None if the subreddit is not valid or an error occurred
-
-if __name__ == '__main__':
-    import sys
-    if len(sys.argv) < 2:
-        print("Please pass an argument for the subreddit to search.")
-    else:
-        top_ten(sys.argv[1])
+if __name__ == "__main__":
+    top_ten(argv[1])
